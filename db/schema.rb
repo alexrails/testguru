a192adcc925c01bc467c7c156b1b05115d1a9ce7
Alex <alexrails@yandex.ru>
2020-02-29 19:23:00 +0300

    Add default values to attributes level and correct

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_154833) do
+ActiveRecord::Schema.define(version: 2020_02_29_161222) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "correct", null: false
+    t.boolean "correct", default: false, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_154833) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

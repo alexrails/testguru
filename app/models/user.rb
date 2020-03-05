3e7938b37614c418c9803b2c9bd136b7b0c65104
Alex <alexrails@yandex.ru>
2020-03-05 23:03:10 +0300

    Add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_passed_tests
   has_many :tests, through: :user_passed_tests
+  has_many :author_tests, class_name: "Test"
   def test_passage(level)
     tests.where(level: level)
   end

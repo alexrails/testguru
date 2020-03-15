35ff2d5ff748b20ce4b136c0ca4f230abc5e3cd3
Alex <korzun.alex@mail.ru>
2020-03-15 19:13:48 +0300

    Revert "Task 6"

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-#  get 'tests/index'
-#  get 'tests/new'
-#  get 'tests/create'
-#  get 'tests/show'
-#  get 'tests/destroy'
-#  get 'questions/index'
-#  get 'questions/show'
-#  get 'questions/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tests do
-    resources :questions, shallow: true
-  end
 end

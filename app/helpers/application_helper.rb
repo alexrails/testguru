630fe65baa161a7092c1b22a18b73a14fe1b5049
Alex <alexrails@yandex.ru>
2020-03-18 23:27:23 +0300

    Add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def current_year
+    Date.current.year
+  end
+
+  def github_url(author, repo)
+    content_tag(:a, 'project', href: "https://github.com/#{author}/#{repo}", target: '_blank')
+  end
+
 end

42b7246d066c15291f7b65f6d73b4e01c18f3612
Alex <alexrails@yandex.ru>
2020-04-21 23:15:38 +0300

    add validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :tests, through: :user_passed_tests
   has_many :author_tests, class_name: "Test"
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 80 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   has_secure_password
 
   def test_passage(level)

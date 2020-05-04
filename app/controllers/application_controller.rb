c35804d46fd786e537e7d6edfc6f32673489bc96
Alex <alexrails@yandex.ru>
2020-05-04 21:46:51 +0300

    Add first_name and last_name to form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-
+  before_action :configure_permitted_params, if: :devise_controller?
   private
 
   def after_sign_in_path_for(user)
     user.is_a?(Admin) ? admin_tests_path : root_path
+  end
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
 end

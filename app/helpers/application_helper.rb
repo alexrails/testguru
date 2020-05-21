2d6f7cf7966341a501a7525b355882fc4c89b618
Alex <alexrails@yandex.ru>
2020-05-22 00:32:42 +0300

    Add bootstrap styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 
+  ALERT_TYPE = {
+    notice: 'alert-info',
+    success: 'alert-success',
+    error: 'alert-danger',
+    alert: 'alert-warning'
+  }
+
   def current_year
     Date.current.year
   end
@@ -8,4 +15,7 @@ module ApplicationHelper
     content_tag(:a, 'project', href: "https://github.com/#{author}/#{repo}", target: '_blank')
   end
 
+  def alert_style(type)
+    ALERT_TYPE.fetch(type.to_sym, 'alert-info')
+  end
 end

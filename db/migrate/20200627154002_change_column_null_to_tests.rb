2e119d55723d6c403711af3a01d7c7effb465ca5
Alex <alexrails@yandex.ru>
2020-06-27 18:44:20 +0300

    Change column_null in category_id

---
+++ db/migrate/20200627154002_change_column_null_to_tests.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNullToTests < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :tests, :category_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_183439) do
+ActiveRecord::Schema.define(version: 2020_06_27_154002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 2020_06_04_183439) do
 
   create_table "gists", force: :cascade do |t|
     t.string "url"
-    t.integer "user_id"
-    t.integer "question_id"
+    t.bigint "user_id"
+    t.bigint "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_gists_on_question_id"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_183439) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
-    t.integer "category_id", null: false
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_183439) do
     t.string "first_name"
     t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["type"], name: "index_users_on_type"
   end
